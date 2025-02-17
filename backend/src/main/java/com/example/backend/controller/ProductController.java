10a519b1d2ca93b5c29029824b91129bf09eea10
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-02-17 23:11:44 +0530

    added endpoint for getting a product by id and formatted the date of a product

--- backend/src/main/java/com/example/backend/controller/ProductController.java
+++ backend/src/main/java/com/example/backend/controller/ProductController.java
@@ -3,26 +3,34 @@ package com.example.backend.controller;
 import com.example.backend.model.Product;
 import com.example.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/a")
-    public String greet(){
-        return "Hello World";
-    }
-
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
+    }
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+
+        Product product = service.getProductById(id);
+        if(product!=null){
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 }
