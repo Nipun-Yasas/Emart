3d5c922d117543037ca92965b7e18d411d7819b8
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2025-02-17 23:45:54 +0530

    modified product entity for add images and created endpoints to add products.test them with after creating the frontend

--- backend/src/main/java/com/example/backend/model/Product.java
+++ backend/src/main/java/com/example/backend/model/Product.java
@@ -1,10 +1,7 @@
 package com.example.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
@@ -31,6 +28,11 @@ public class Product {
     private Date releasedate;
     private boolean available;
     private int quantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 
 }
 
